9a74733f2d16905f722f986e60a329e6ee07fb17
Michael <michael18gm@gmail.com>
2026-06-22 19:05:01 -0600

    Modificando la misma linea

--- PruebaGit/src/hello3.java
+++ PruebaGit/src/hello3.java
@@ -2,6 +2,6 @@ public class hello3 {
  //Esto es un comentario
     //Version dos del comentario V3
  public static void main(String[] args) {
-     System.out.println("Michael");
+     System.out.println("Michael Alejandro");
  }
 }
