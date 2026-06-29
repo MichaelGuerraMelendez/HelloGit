906aea5ac986cc8f1aa1862846104e305f8ac470
MichaelEscritorioPC <michaelguerram@outlook.es>
2026-06-28 22:36:32 -0600

    Comit desde pc escritorio

--- PruebaGit/src/hello3.java
+++ PruebaGit/src/hello3.java
@@ -2,6 +2,6 @@ public class hello3 {
  //Esto es un comentario
     //Version dos del comentario V3
  public static void main(String[] args) {
-     System.out.println("Michael Alejandro");
+     System.out.println("Michael Alejandro Guerra Melendez");
  }
 }
